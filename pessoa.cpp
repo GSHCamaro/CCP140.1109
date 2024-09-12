587a652f6b20058f780a4ab3fa9eaec2756f66cb
GSHCamaro <149434698+GSHCamaro@users.noreply.github.com>
2024-09-12 00:53:40 +0000

    adicionado pessoa e alterado a main

---
+++ pessoa.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Pessoa{
+    private:
+    string nome;
+    string sobrenome;
+    long cpf;
+    public:
+    void setNome(string nome){
+        this->nome = nome;
+    }
+    void setSobrenome(string sobrenome){
+        this->sobrenome = sobrenome;
+    }
+    void setCpf(long cpf){
+        this->cpf = cpf;
+    }
+    string getSobrenome(){
+        return sobrenome;
+    }
+    string getNome(){
+        return this->nome;
+  }
+    long getCpf(){
+        return cpf;
+  }
+};
